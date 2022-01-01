8ba2a813e49cac49671dba78f2003d0efbbc58ac
Beasley9 <57361391+Beasley9@users.noreply.github.com>
2021-12-31 19:02:18 -0600

    Create LucidLonghorn.sol

---
+++ contracts/LucidLonghorn.sol
@@ -0,0 +1,25 @@
+/*
+SPDX-License-Identifier: MIT
+*/
+
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BevoPunk is Ownable, ERC721 {
+
+	constructor()
+	ERC721("LucidLonghorn", "LUCL")
+	{}
+
+	function mint(address account) public returns (bool) {
+		_mint(account, 1);
+		return true;
+	}
+
+	function transfer(address recipient, uint256 tokenId) public returns (bool) {
+		_transfer(msg.sender, recipient, tokenId);
+	}
+}

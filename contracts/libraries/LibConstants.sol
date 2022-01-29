866ef973c0c43f7a08868946a4ea8d811e37b302
Beasley9 <57361391+Beasley9@users.noreply.github.com>
2022-01-29 11:19:18 -0600

    Update LibConstants.sol

--- contracts/libraries/LibConstants.sol
+++ contracts/libraries/LibConstants.sol
@@ -6,17 +6,15 @@ pragma experimental ABIEncoderV2;
 
 library LibConstants {
 
-	uint64 private constant BASE_VALUE = 50000000000000000;
-
 	uint64 private constant BRONZE_VALUE = 100000000000000000;
 
-	uint64 private constant SILVER_VALUE = 500000000000000000;
+	uint64 private constant SILVER_VALUE = 250000000000000000;
 
 	uint64 private constant GOLD_VALUE = 1000000000000000000;
 
-	function base() internal pure returns (uint64) {
-		return BASE_VALUE;
-	}
+	uint64 private constant LEGENDARY_VALUE = 3000000000000000000;
+
+	uint128 private constant MAX_VALUE = 20000000000000000000;
 
 	function bronze() internal pure returns (uint64) {
 		return BRONZE_VALUE;
@@ -28,5 +26,13 @@ library LibConstants {
 
 	function gold() internal pure returns (uint64) {
 		return GOLD_VALUE;
+	}
+
+	function legendary() internal pure returns (uint64) {
+		return LEGENDARY_VALUE;
+	}
+
+	function maxLegendary() internal pure returns (uint128) {
+		return MAX_VALUE;
 	}
 }
